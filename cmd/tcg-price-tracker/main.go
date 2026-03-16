176f42e4162aa0b24a5041461628b4db7742acb1
Rayan Butt <rayrb2001@gmail.com>
2026-03-16 22:32:10 +0000

    scraper working as intended through pricecharting.com

--- cmd/tcg-price-tracker/main.go
+++ cmd/tcg-price-tracker/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/RayRB21/tcg-price-tracker/internal/scraper"
+)
+
+func main() {
+	cards, err := scraper.FetchAllCards()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("\nFetched %d cards total\n\n", len(cards))
+	for _, c := range cards {
+		fmt.Printf("%-50s $%.2f  %s%s%%\n",
+			c.Name, c.Price, c.PriceChangeSign,
+			strconv.FormatFloat(c.PriceChangePercentage, 'f', 1, 64))
+	}
+}
